4e8257fd091dfd3844cfb8a8766bf89993fdcd29
Javier Gomez <javier.gov@outlook.es>
2023-06-16 00:16:22 -0600

    [UPDATE] Getters y setters de la clase persona y metodo toString

--- src/Persona.java
+++ src/Persona.java
@@ -12,9 +12,57 @@ public class Persona {
 
     //Constructor con argumentos
     public Persona(String nombre, String telefono, String email){
-        this(); //Se llama de manera explicita el constructor vacio 
+        this(); //Se llama de manera explicita el constructor vacio
         this.nombre = nombre;
         this.telefono = telefono;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan Pérez", "123456789", "juan@mail.com");
+        System.out.println(persona1);
+    }
+
 }
